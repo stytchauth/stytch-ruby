71f1cd16a3e9bfed316a7cbec6b1470879121403
Grace Baelen-King <70169804+grace-stytch@users.noreply.github.com>
2020-12-14 15:19:41 -0800

    Remove Email Verify Endpoints (#11)
    * remove email verify
    
    * bump version

--- lib/stytch/endpoints/email.rb
+++ lib/stytch/endpoints/email.rb
@@ -3,25 +3,6 @@ module Stytch
     module Email
       PATH = "/v1/emails".freeze
 
-      def send_email_verification(
-          user_id:,
-          email_id:,
-          magic_link_url:,
-          expiration_minutes:
-      )
-        request = {
-            user_id: user_id,
-            magic_link_url: magic_link_url,
-            expiration_minutes: expiration_minutes
-        }
-
-        post("#{PATH}/#{email_id}/send_verification", request)
-      end
-
-      def verify_email(token:)
-        post("#{PATH}/#{token}/verify", {})
-      end
-
       def delete_email(user_id:, email_id:)
         delete("#{PATH}/#{email_id}/users/#{user_id}")
       end
