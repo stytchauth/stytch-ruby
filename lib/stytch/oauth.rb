8c9ff79db7835827898d5ab1601856c2526daa18
Danny Thomson <danny@stytch.io>
2021-09-14 14:17:37 -0700

    Make session_management_type optional

--- lib/stytch/oauth.rb
+++ lib/stytch/oauth.rb
@@ -14,12 +14,12 @@ module Stytch
 
     def authenticate(
       token:,
-      session_management_type:,
+      session_management_type: {},
     )
       request = {
         token: token
-        session_management_type: session_management_type
       }
+      request[:session_management_type] = session_management_type if session_management_type != {}
 
       post_request("#{PATH}/authenticate", request)
     end
