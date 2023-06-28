d76666f32e8b6fbdd1d8c3e7474e5c33f88c2660
jonathan schatz <jon@tread.io>
2023-06-28 16:54:06 -0700

    fix StytchB2B::Client (fixes #81)

---
+++ spec/stytch/b2b_client_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+RSpec.describe StytchB2B::Client do
+  it 'accepts production config' do
+    _client = StytchB2B::Client.new(
+      env: :live,
+      project_id: 'project-live-00000000-0000-0000-0000-000000000000',
+      secret: 'secret-live-11111111-1111-1111-1111-111111111111',
+    )
+  end
+
+  it 'accepts testing config' do
+    _client = StytchB2B::Client.new(
+      env: :test,
+      project_id: 'project-test-00000000-0000-0000-0000-000000000000',
+      secret: 'secret-test-11111111-1111-1111-1111-111111111111',
+    )
+  end
+
+  it 'accepts development config' do
+    _client = StytchB2B::Client.new(
+      env: 'http://localhost:8000',
+      project_id: 'project-test-00000000-0000-0000-0000-000000000000',
+      secret: 'secret-test-11111111-1111-1111-1111-111111111111',
+    )
+
+    _client = StytchB2B::Client.new(
+      env: 'https://foo.stytch.test',
+      project_id: 'project-test-00000000-0000-0000-0000-000000000000',
+      secret: 'secret-test-11111111-1111-1111-1111-111111111111',
+    )
+  end
+
+  it 'infers environment when not given' do
+    _client = StytchB2B::Client.new(
+      project_id: 'project-test-00000000-0000-0000-0000-000000000000',
+      secret: 'secret-test-11111111-1111-1111-1111-111111111111',
+    )
+  end
+end
