19ec14d4f51095ca2b9398aac490fcb6b5602b3b
Ollie Elmgren <111321901+ollie-stytch@users.noreply.github.com>
2025-04-28 16:32:45 -0700

    Accept BaseURL as Issuer (#171)
    * add base URL as accepted issuer
    
    * lint
    
    * version

--- spec/stytch/sessions_spec.rb
+++ spec/stytch/sessions_spec.rb
@@ -1,9 +1,21 @@
 # frozen_string_literal: true
 
+# Simple connection class for testing
+class TestConnection
+  attr_reader :api_host
+
+  def initialize(api_host = 'https://test.stytch.com')
+    @api_host = api_host
+  end
+end
+
 RSpec.describe Stytch::Sessions do
   it 'correctly decodes a JWT' do
     project_id = 'project-test-00000000-0000-0000-0000-000000000000'
-    sessions = Stytch::Sessions.new(nil, project_id) # the methods we're calling don't require a connection
+
+    # Use the TestConnection class with default api_host
+    connection = TestConnection.new
+    sessions = Stytch::Sessions.new(connection, project_id)
 
     kid = 'jwk-test-00000000-0000-0000-0000-000000000000'
     headers = { kid: kid }
@@ -50,9 +62,65 @@ RSpec.describe Stytch::Sessions do
     expect(sessions.authenticate_jwt_local(token)).to eq(expected)
   end
 
+  it 'correctly decodes a JWT with custom issuer' do
+    project_id = 'project-test-00000000-0000-0000-0000-000000000000'
+    custom_api_host = 'https://api.custom-domain.com'
+
+    # Use the TestConnection class with custom api_host
+    connection = TestConnection.new(custom_api_host)
+    sessions = Stytch::Sessions.new(connection, project_id)
+
+    kid = 'jwk-test-00000000-0000-0000-0000-000000000000'
+    headers = { kid: kid }
+
+    now = Time.now
+    now_timestamp = format_timestamp(now)
+    claims = {
+      'https://stytch.com/session' => {
+        'started_at' => now_timestamp,
+        'last_accessed_at' => now_timestamp,
+        'expires_at' => (now + 3600).to_datetime.iso8601,
+        'attributes' => { 'user_agent' => '', 'ip_address' => '' },
+        'authentication_factors' => [
+          {
+            'delivery_method' => 'email',
+            'email_factor' => {
+              'email_address' => 'sandbox@stytch.com',
+              'email_id' => 'email-live-cca9d7d0-11b6-4167-9385-d7e0c9a77418'
+            },
+            'last_authenticated_at' => now_timestamp,
+            'type' => 'magic_link'
+          }
+        ],
+        'id' => 'session-live-e26a0ccb-0dc0-4edb-a4bb-e70210f43555'
+      },
+      'sub' => 'user-live-fde03dd1-fff7-4b3c-9b31-ead3fbc224de',
+      'iat' => now.to_i,
+      'nbf' => now.to_i,
+      'exp' => now.to_i + 5 * 60, # five minutes
+      'iss' => custom_api_host, # Using the custom domain as issuer
+      'aud' => [project_id]
+    }
+    jwk = JWT::JWK.new(OpenSSL::PKey::RSA.new(2048), kid)
+    token = JWT.encode(claims, jwk.keypair, 'RS256', headers)
+
+    # patch the jwks_loader method so that it uses the JWK we just created
+    # instead of calling the API directly
+    patch_jwks_loader = lambda do |_options|
+      { 'keys' => [jwk.export] }
+    end
+    sessions.instance_variable_set(:@jwks_loader, patch_jwks_loader)
+    expected = sessions.marshal_jwt_into_session(claims)
+
+    expect(sessions.authenticate_jwt_local(token)).to eq(expected)
+  end
+
   it 'marshals JWT into session (new format)' do
     project_id = 'project-test-00000000-0000-0000-0000-000000000000'
-    sessions = Stytch::Sessions.new(nil, project_id) # the methods we're calling don't require a connection
+
+    # Use the TestConnection class with default api_host
+    connection = TestConnection.new
+    sessions = Stytch::Sessions.new(connection, project_id)
 
     now = Time.utc(2022, 5, 3, 18, 51, 41)
     claims = jwt_claims(project_id, now)
@@ -64,7 +132,10 @@ RSpec.describe Stytch::Sessions do
 
   it 'marshals JWT into session (old format)' do
     project_id = 'project-test-00000000-0000-0000-0000-000000000000'
-    sessions = Stytch::Sessions.new(nil, project_id) # the methods we're calling don't require a connection
+
+    # Use the TestConnection class with default api_host
+    connection = TestConnection.new
+    sessions = Stytch::Sessions.new(connection, project_id)
 
     now = Time.utc(2022, 5, 3, 18, 51, 41)
     claims = jwt_claims(project_id, now)
