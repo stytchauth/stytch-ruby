00dc96929e0695437ead613871c12d3124333c2d
Brandon Gier <bgier@stytch.com>
2025-07-25 14:49:13 -0700

    Other instances

--- lib/stytch/b2b_idp.rb
+++ lib/stytch/b2b_idp.rb
@@ -102,10 +102,11 @@ module StytchB2B
 
       url = "/v1/public/#{@project_id}/oauth2/introspect"
       jwt_response = post_request(url, data, headers)
+
       return nil unless jwt_response['active']
 
-      custom_claims = res.reject { |k, _| @non_custom_claim_keys.include?(k) }
-      organization_claim = res['https://stytch.com/organization']
+      custom_claims = jwt_response.reject { |k, _| @non_custom_claim_keys.include?(k) }
+      organization_claim = jwt_response['https://stytch.com/organization']
       organization_id = organization_claim['organization_id']
       scope = jwt_response['scope']
 
