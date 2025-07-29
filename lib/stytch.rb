2228283bd600d790f426d2918e466b1e1250eff1
bgier-stytch <169087791+bgier-stytch@users.noreply.github.com>
2025-07-29 09:32:09 -0700

    [Auth-5664] Consumer IDP (#191)
    * Add rbac
    
    * Add sessions
    
    * Restore docs
    
    * Update
    
    * Move rbac local back
    
    * OOp
    
    * Update
    
    * Remove extra line
    
    * Add in authorization_check to consumer sessions file, and add in a test
    
    * Wow cursor can do lint refactors
    
    * Initial cursor only pass
    
    * Update tests
    
    * Yay cursor lint
    
    * Update
    
    * Oop
    
    * Consumer
    
    * Remove integration test file
    
    * Lint

--- lib/stytch.rb
+++ lib/stytch.rb
@@ -4,6 +4,7 @@ require 'faraday'
 
 require_relative 'stytch/b2b_client'
 require_relative 'stytch/client'
+require_relative 'stytch/idp'
 require_relative 'stytch/method_options'
 require_relative 'stytch/middleware'
 require_relative 'stytch/version'
